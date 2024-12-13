f82f3f718d019805de3c51dc4b13fed7970a6de6
Mahdee Hassan Sami <mahdeehassansami@gmail.com>
2024-12-13 21:55:32 +0600

    finilized the add money and cashout function

--- js/home.js
+++ js/home.js
@@ -2,7 +2,53 @@
 document.getElementById('btn-add-money')
 // step -1 Add an event handler to the add money button inside the form
 .addEventListener('click', function(event){
-    event.preventDefault
+    event.preventDefault();
     const addMoneyInput=document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
+    const pinNumberInput=document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+    if(pinNumberInput==='1234'){
+        console.log('adding money to your account');
+        const balance=document.getElementById('balance').innerText
+        console.log("balance", balance);
+        const addMoneynumber=parseFloat(addMoneyInput);
+        const balanceNumber=parseFloat(balance);
+        console.log("balance", balanceNumber);
+
+        const newBalance = addMoneynumber + balanceNumber;
+        console.log(newBalance);
+        document.getElementById('balance').innerText=newBalance;
+
+    }
+    else{
+        console.log('Failed to add money');
+    }
+})
+
+
+// Cash out
+document.getElementById('btn-cashout')
+// step -1 Add an event handler to the add money button inside the form
+.addEventListener('click', function(event){
+    event.preventDefault();
+    const cashOutInput=document.getElementById('input-cashout').value;
+    console.log(cashOutInput);
+    const pinNumberInput=document.getElementById('input-cashoutpin-number').value;
+    console.log(pinNumberInput);
+    if(pinNumberInput==='1234'){
+        console.log('Cash out money');
+        const balance=document.getElementById('balance').innerText
+        console.log("balance", balance);
+        const cashOutNumber=parseFloat(cashOutInput);
+        const balanceNumber=parseFloat(balance);
+        console.log("balance", balanceNumber);
+
+        const newBalance = -cashOutNumber + balanceNumber;
+        console.log(newBalance);
+        document.getElementById('balance').innerText=newBalance;
+
+    }
+    else{
+        console.log('Failed to Cash Out money');
+    }
 })
